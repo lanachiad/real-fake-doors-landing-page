5f497acaf916b806a61d304ba61caccffb2b5a2e
Lana Chiad <lanachiad7@gmail.com>
2018-02-16 18:46:56 -0500

    Toggle mobile nav=links on click

--- app/js/mobile_nav.js
+++ app/js/mobile_nav.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-  $("#mobile.nav").click(function() {
-    if ($("#mobile-nav-link").css(("display", "none"))) {
-      $("#mobile-nav-link").slideDown("slow");
-    } else {
-      $("#mobile-nav-link").hide();
-    }
+  $("#mobile-nav").click(function(e) {
+    $("#mobile-nav-links").toggleClass("disappear");
   });
 });
